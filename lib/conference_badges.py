1fee93490f9ca8c9487a55975af9cbf66ffca058
Eileen <eileenburdekin@proton.me>
2023-12-14 15:27:08 -0800

    refactor using list comprehension

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -3,19 +3,24 @@ def badge_maker(name):
 
 
 def batch_badge_creator(names):
-    badges = []
-    for name in names:
-        badge = badge_maker(name)
-        badges.append(badge)
-    return badges
+    # badges = []
+    # for name in names:
+    #     badge = badge_maker(name)
+    #     badges.append(badge)
+    # return badges
+    return [badge_maker(name) for name in names]
 
 
 def assign_rooms(names):
-    rooms = []
-    for index, name in enumerate(names):
-        room_assignment = f"Hello, {name}! You'll be assigned to room {index+1}!"
-        rooms.append(room_assignment)
-    return rooms
+    # rooms = []
+    # for index, name in enumerate(names):
+    #     room_assignment = f"Hello, {name}! You'll be assigned to room {index+1}!"
+    #     rooms.append(room_assignment)
+    # return rooms
+    return [
+        f"Hello, {name}! You'll be assigned to room {index+1}!"
+        for index, name in enumerate(names)
+    ]
 
 
 def printer(names):
